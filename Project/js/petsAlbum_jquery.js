5c94edb0501ff315821939e3855823fde57b4b7a
Miffy Chen <mchen15@bu.edu>
2019-12-09 18:32:52 -0500

    Fixed photo large size, Added calculator, Added macbook, Added course_eval

--- Project/js/petsAlbum_jquery.js
+++ Project/js/petsAlbum_jquery.js
@@ -63,28 +63,3 @@ function appendData(data) {
 
     $("#photos").append(container_album);
 };
-
-
-window.onload = function() {
-
-    let modal = document.getElementById("myModal");
-    let modalImg = document.getElementById("img01");
-    let captionText = document.getElementById("caption");
-
-    $("#pet-album").on("click", ".polaroid", function (event) {
-        let img = this.querySelector("img");
-        let figcaption_p = this.querySelector("figcaption p");
-
-        modal.style.display = "block";
-        modalImg.src = img.src;
-        captionText.innerHTML = figcaption_p.innerHTML;
-    });
-
-    // Get the <span> element that closes the modal
-    let span = document.getElementsByClassName("close")[0];
-
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function () {
-        modal.style.display = "none";
-    }
-};
